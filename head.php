a838d1a9ddc4a7ca5a2dff25b663826ef09ab63c
Jume <devjume@mailbox.org>
2022-03-24 18:52:57 +0200

    Lisätty css tiedosto

--- head.php
+++ head.php
@@ -1,3 +1,7 @@
+<?php
+define('ROOT', 'http://localhost/moviedb-backend');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,10 +9,10 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>App</title>
+  <title>Elokuva Tietokanta</title>
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+  <link rel="stylesheet" href="<?php echo ROOT . '/style.css' ?>">
 </head>
 
 <body>
