d8e5b55a7f15831b280bd05af3c6cb832c71c68f
SAKU PENTTILÄ <sakuinenpenttila@gmail.com>
2022-03-24 11:49:55 +0200

    movies.php taulukko. Elokuva lisätään kantaan -> uusi rivi taulukkoon

--- movies.php
+++ movies.php
@@ -2,15 +2,32 @@
 require_once "./inc/functions.php";
 require_once "./inc/headers.php";
 
-try {
-  $db = openDB();
-  
-  $sql = "SELECT * FROM `elokuva`";
-  $query = $db->query($sql);
-  $result = $query->fetchAll();
+  echo "<table>";
+  echo "<style>
+  table, th, td {
+    border:1px solid black;
+  }
+  </style>";
+  echo "<tr>
+        <th>Nimi</th>
+        <th>Tulovuosi</th>
+        <th>Kesto</th>
+        <th>Kieli</th>
+        <th>Ikäraja</th>
+        </tr>";
+  createTableRow();
+  echo "</table>";
 
-  print json_encode($result);
 
-} catch (PDOException $e) {
-  returnError($e);
+function createTableRow() {
+  $pdo = openDB();
+  $sql = "SELECT nimi, vuosi, kesto, kieli, ikaraja FROM elokuva";
+  $elokuvat = $pdo->query($sql);
+  if ($elokuvat->rowCount() > 0 ) {
+      echo '<tr>';
+      while($row = $elokuvat->fetch()) {
+          echo '<td>' . $row["nimi"] . '</td>' . '<td>' . $row["vuosi"] . '</td>' . '<td>' . $row["kesto"] . '</td>' . '<td>' . $row["kieli"] . '</td>' . '<td>' . $row["ikaraja"] . '</td>' ;
+      }
+      echo "</tr>";
+  }
 }
