e8368def321c164adfcd2edda80f3b75e9d07912
Jume <devjume@mailbox.org>
2022-03-24 11:37:51 +0200

    Otettu json data pois käytöstä

--- inc/headers.php
+++ inc/headers.php
@@ -4,7 +4,7 @@ header('Access-Control-Allow-Credentials:true');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Access-Control-Max-Age: 3600');
-header('Content-type: application/json');
+#header('Content-type: application/json');
 
 if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
   if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
