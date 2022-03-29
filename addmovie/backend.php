d087df6aaf67e5924bec26a2d3a93ea63a9a8cd7
Jume <devjume@mailbox.org>
2022-03-29 22:40:02 +0300

    Elokuva voidaan lisätä uudella genrellä ja ohjaajalla

--- addmovie/backend.php
+++ addmovie/backend.php
@@ -37,8 +37,12 @@ try {
   if (!$db) {
     echo "Database connection Failed!";
   }
-  
-  ## TEE TÄSSÄ ENSIN GENRE ID JA OHJAAJA ID TARKASTUS JA MAHDOLLINEN LUONTI
+
+  # TARKASTA LÖYTYYKÖ ELOKUVA JO TIETOKANNASTA
+  # LISÄÄ NÖYTTELIJÄT JA ROOLIT
+
+  $ohjaaja_id = luoOhjaaja($db, $ohjaaja);
+  $genre_id = luoGenre($db, $genre);
 
   $sql = "INSERT INTO elokuva (nimi, vuosi, kesto, kieli, ohjaaja_id, ikaraja, genre_id) VALUES (?,?,?,?,?,?,?)";
 
@@ -51,13 +55,10 @@ try {
   $pdoStatement->bindParam(6, $ikaraja);
   $pdoStatement->bindParam(7, $genre_id);
 
-  #$pdoStatement->execute();
-
-  # Tee tästä paremi -> palauta vain json ja tee frontendissä sen näyttäminen -> poista page auto refresh after post
-  #header("Location: http://localhost/xx/workTime.php", true, 301);
-  #echo "Käyttäjälle:" . $personid . " lisättiin uusi työaika merkintä.";
-  #http_response_code(201);
-  #print json_encode((array("success" => "Käyttäjälle: " . $personid . " lisättiin uusi työaika merkintä.")));
+  $pdoStatement->execute();
+  
+  http_response_code(201);
+  print json_encode((array("success" => "Elokuva: " . $nimi . " on lisätty tietokantaan")));
   exit();
   #header('Content-type: text/html');
 
@@ -65,31 +66,43 @@ try {
   returnError($e);
 }
 
+function luoOhjaaja($db, $ohjaaja)
+{
+  $sqlCheckGenre = "SELECT id FROM `ohjaaja` WHERE `nimi` = ?";
+  $pdoGenreExits = $db->prepare($sqlCheckGenre);
+  $pdoGenreExits->bindParam(1,  $ohjaaja);
+  $pdoGenreExits->execute();
+  $ohjaaja_id = $pdoGenreExits->fetchColumn();
 
+  # Jos syötettyä ohjaajaa ei ole tietokannassa, Sellainen luodaan.
+  if (empty($ohjaaja_id)) {
+    $sqlCreateGenre = "INSERT INTO ohjaaja(nimi) VALUES (?)";
+    $pdoCreateGenre = $db->prepare($sqlCreateGenre);
+    $pdoCreateGenre->bindParam(1, $ohjaaja);
+    $pdoCreateGenre->execute();
+    $ohjaaja_id = $db->lastInsertId();
+    return $ohjaaja_id;
+  }
+  return $ohjaaja_id;
+}
 
-#echo $nimi;
-#echo "<br>";
-#echo $vuosi;
-#echo "<br>";
-#echo $kesto;
-#echo "<br>";
-#echo $kieli;
-#echo "<br>";
-#echo $ikaraja;
-#echo "<br>";
-#echo $ohjaaja;
-#echo "<br>";
-#echo $genre;
-#echo "<br>";
-#echo "<br>";
-#
-#foreach($nayttelijat as $x) {
-#  echo $x['nimi'];
-#  echo "<br>";
-#  echo $x['rooli'];
-#  echo "<br>";
-#  echo $x['sukupuoli'];
-#  echo "<br>";
-#  echo "<br>";
-#}
+function luoGenre($db, $genre) {
+  $sqlCheckGenre = "SELECT id FROM `genre` WHERE `nimi` = ?";
+  $pdoGenreExits = $db->prepare($sqlCheckGenre);
+  $pdoGenreExits->bindParam(1,  $genre);
+  $pdoGenreExits->execute();
+  $genre_id = $pdoGenreExits->fetchColumn();
+
+  # Jos syötettyä genreä ei ole tietokannassa, Sellainen luodaan.
+  if (empty($genre_id)) {
+    $sqlCreateGenre = "INSERT INTO genre(nimi) VALUES (?)";
+    $pdoCreateGenre = $db->prepare($sqlCreateGenre);
+    $pdoCreateGenre->bindParam(1, $genre);
+    $pdoCreateGenre->execute();
+    $genre_id = $db->lastInsertId();
+    return $genre_id;
+  }
+  return $genre_id;
+}
+
 ?>
