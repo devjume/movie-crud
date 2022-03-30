de3d7ccc368e34787e3122ed1447058f22730d0d
Jume <devjume@mailbox.org>
2022-03-30 13:57:15 +0300

    Lisätty linkkejä etusivulle

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
 include "./head.php";
 echo '<h1>Index</h1>';
+echo "<br>";
+echo "<a href='./addmovie/addmovie.php'>Lisää elokuva</a>";
+echo "<br>";
+echo "<a href='movies.php'>Elokuvat</a>";
+echo "<br>";
+echo "<a href='./single.php?id=1'>Yksittäinen elokuva (Muuta id -> katso url '?id=2'</a>";
+echo "<br>";
+
 include "./foot.php";
