254d597a61b477386a004f96ecf681067c86943b
Jume <devjume@mailbox.org>
2022-04-22 11:20:38 +0300

    Muokattu etusivun rakennetta ja otettu käyttöön elokuva kortit

--- public/index.php
+++ public/index.php
@@ -1,27 +1,31 @@
 <?php
-include TEMPLATES_DIR."head.php";
-require_once MODULES_DIR."/inc/functions.php";
-require_once MODULES_DIR ."/inc/headers.php";
-echo '<h1>Index</h1>';
-echo "<br>";
+include TEMPLATES_DIR . "head.php";
+require_once MODULES_DIR . "/inc/functions.php";
+require_once MODULES_DIR . "/inc/headers.php";
+include TEMPLATES_DIR . "movieCard.php";
+?>
 
-try {
-  createTableRow();
-} catch (PDOException $e) {
-  returnError($e);
-}
+<div class="container">
+  <h1>Elokuva tietokanta </h1>
+  <div class="row row-cols-3 row-cols-md-3 g-1">
+    <?php
+    try {
+      $sql = "SELECT * FROM `elokuva` where kuva_url IS not NULL ORDER BY RAND () limit 5";
+      $pdo = openDB();
+      $elokuvat = $pdo->query($sql);
 
-function createTableRow() {
-$sql = "SELECT * FROM `elokuva` where kuva_url IS not NULL ORDER BY RAND () limit 5";
-$pdo = openDB();
-$elokuvat = $pdo->query($sql);
+      if ($elokuvat->rowCount() > 0) {
+        while ($row = $elokuvat->fetch()) {
+          createSingleCard($row);
+        }
+      }
+    } catch (PDOException $e) {
+      returnError($e);
+    }
+    ?>
+  </div>
+</div>
 
-  if ($elokuvat->rowCount() > 0 ) {
-    echo "<ul>";
-  while ( $row = $elokuvat->fetch() ) {
-      echo '<li> <a href="single.php?id='.$row["id"].'">'. '<img class="w-25 p-3; rounded" src="' .$row["kuva_url"]. '"alt=""/>'.'</a>'. 
-    "<h2>". $row["nimi"]. "<h2><h3>". $row["vuosi"]. "<br>". $row["kesto"]. " min<br>Ikäraja: ". $row["ikaraja"]. "</h3></li>";
-  }
-  echo "</ul>";
-}}
-include TEMPLATES_DIR."foot.php";
+<?php
+include TEMPLATES_DIR . "foot.php";
+?>
