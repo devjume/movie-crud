58d32a5c0e9c4a91f122656fc8d24ece6aed7714
Jume <devjume@mailbox.org>
2022-04-22 11:15:00 +0300

    Luoto elokuva kortti template ja otettu se käyttöön kategoria sivuilla

--- public/movies.php
+++ public/movies.php
@@ -2,27 +2,11 @@
 include TEMPLATES_DIR . "head.php";
 require_once MODULES_DIR . "/inc/functions.php";
 require_once MODULES_DIR . "/inc/headers.php";
+
+include TEMPLATES_DIR . "movieCard.php";
 ?>
 <div class="container">
 <h1>Kaikki elokuvat </h1>
-
-<!-- <table>
-  <style>
-    table,
-    td,
-    th {
-      border: 1px solid black;
-    }
-  </style>
-  <tr>
-    <th>Nimi</th>
-    <th>Tulovuosi</th>
-    <th>Kesto(min)</th>
-    <th>Kieli</th>
-    <th>Ikäraja</th>
-    <th>Ohjaaja</th>
-  </tr>
-</table> -->
   <div class="row row-cols-3 row-cols-md-3 g-1">
     <?php
 
@@ -34,43 +18,25 @@ require_once MODULES_DIR . "/inc/headers.php";
       exit;
     }
 
-
-
     if ($id === 0) {
       $sql = "SELECT elokuva.id as id, elokuva.nimi as elokuva, vuosi, kesto, kieli, ikaraja, kuva_url, ohjaaja.nimi as ohjaaja FROM elokuva
-left join ohjaaja on elokuva.ohjaaja_id = ohjaaja.id";
+              left join ohjaaja on elokuva.ohjaaja_id = ohjaaja.id";
     } else {
       $sql = "SELECT elokuva.id as id, elokuva.nimi as elokuva, vuosi, kesto, kieli, ikaraja, kuva_url, ohjaaja.nimi as ohjaaja FROM elokuva
-left join ohjaaja on elokuva.ohjaaja_id = ohjaaja.id WHERE elokuva.genre_id = $id";
+              left join ohjaaja on elokuva.ohjaaja_id = ohjaaja.id WHERE elokuva.genre_id = $id";
     }
 
     $pdo = openDB();
     $elokuvat = $pdo->query($sql);
     if ($elokuvat->rowCount() > 0) {
       while ($row = $elokuvat->fetch()) {
-        echo '<div class="col-3"> <div class="card" style="width: 18rem;">
-      <img src="' . $row["kuva_url"] . '"class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">' . $row["elokuva"] . '</h5>
-    <ul class="list-group list-group-flush">
-    <li class="list-group-item">Vuosi: ' . $row["vuosi"] . '</li>
-    <li class="list-group-item">Kesto: ' . $row["kesto"] . ' min'  . '</li>
-    <li class="list-group-item">Kieli: ' . $row["kieli"] . '</li>
-    <li class="list-group-item">Ikäraja: ' . $row["ikaraja"] . '</li>
-    <li class="list-group-item">Ohjaaja: ' . $row["ohjaaja"] . '</li>
-  </ul>
-  </div>
-</div> </div>';
+        #Kutsuu movieCard.php template tiedoston funktiota, joka luo elokuva kortit
+        createSingleCard($row);
       }
     }
-
-    #echo '<tr>' . '<td>' . '<a href="single.php?id=' . $row["id"] . '">' . $row["elokuva"] . '</a>' . '</td>' . '<td>' . $row["vuosi"] . '</td>' . '<td>' . $row["kesto"] . '</td>' . '<td>' . $row["kieli"] . '</td>' . '<td>' . $row["ikaraja"] . '</td>' . '<td>' . $row["Ohjaaja"] . '</td>' . "</tr>";
-
-
     ?>
-    <!-- </table> -->
   </div>
-  </div>
+</div>
     <?php
     include TEMPLATES_DIR . "foot.php";
     ?>
