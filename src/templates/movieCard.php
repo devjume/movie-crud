eb45b8a18b06b45ccefba6c41b7f56bceac4a77a
ronikauppinen <80982932+ronikauppinen@users.noreply.github.com>
2022-05-02 14:12:02 +0300

    Update movieCard.php
    muutoksia

--- src/templates/movieCard.php
+++ src/templates/movieCard.php
@@ -6,13 +6,13 @@ function createSingleCard($data) {
       <div class="card" style="width: 18rem;">
         <img src=<?php echo $data['kuva_url'] ?> class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title"><?php echo $data["elokuva"] ?></h5>
+          <h5 class="card-title"><?php echo $data["elokuva nimi"] ?></h5>
           <ul class="list-group list-group-flush">
           <li class="list-group-item">Vuosi: <?php echo $data["vuosi"] ?></li>
           <li class="list-group-item">Kesto: <?php echo $data["kesto"] ?> min</li>
           <li class="list-group-item">Kieli: <?php echo $data["kieli"] ?></li>
           <li class="list-group-item">Ikäraja: <?php echo $data["ikaraja"] ?></li>
-          <li class="list-group-item">Ohjaaja: <?php echo $data["ohjaaja"] ?></li>
+          <li class="list-group-item">Ohjaaja: <?php echo $data["ohjaaja nimi"] ?></li>
         </ul>
         <a href="single.php?id= <?php echo $data["id"] ?>" class="stretched-link"></a>
       </div>
